24ef598d0b1210296408c103c3b56def1070852f
Owen Roberts <o.roberts@corgihomeplan.co.uk>
2022-02-03 11:36:25 +0000

    Add integration tests for mongo repository

--- tests/Books.Service.Tests.Integration/Infrastructure/Mongo/MongoBookRepositoryTests.cs
+++ tests/Books.Service.Tests.Integration/Infrastructure/Mongo/MongoBookRepositoryTests.cs
@@ -10,26 +10,75 @@ using Xunit;
 
 namespace Books.Service.Tests.Integration.Infrastructure.Mongo;
 
-public class MongoBookRepositoryTests : BaseIntegrationTest
+[Collection("MongoBookRepositoryTests")]
+public class MongoBookRepositoryTests : BaseIntegrationTest, IAsyncDisposable
 {
     private readonly IBookRepository _booksRepository;
 
     public MongoBookRepositoryTests() : base()
     {
         _booksRepository = ServiceProvider!.GetRequiredService<IBookRepository>();
+        CleanDatabase().GetAwaiter().GetResult();
     }
 
     [Theory, AutoData]
-    public async Task CreateBook(Book expected)
+    public async Task Create(Book expected)
     {
-        var actual = await _booksRepository.CreateAsync(expected);
-
-        actual.Id.Should().Be(1);
-        actual.Title.Should().BeEquivalentTo(expected.Title);
-        actual.Author.Should().BeEquivalentTo(expected.Author);
-        actual.Price.Should().Be(expected.Price);
+        await _booksRepository.CreateAsync(expected);
+        
+        var actual = await _booksRepository.GetBookAsync(expected.Id);
+        
+        actual.Should().NotBeNull();
+        actual!.Id.Should().Be(1);
+        actual.Should().BeEquivalentTo(expected);
 
         await _booksRepository.DeleteAsync(expected.Id);
+    }
+
+    [Theory, AutoData]
+    public async Task Create_BookAlreadyExists_ShouldIncrementId(Book expected)
+    {
+        await _booksRepository.CreateAsync(expected);
+
+        var actual = await _booksRepository.CreateAsync(expected);
+
+        actual.Id.Should().Be(2);
+        actual.Should().BeEquivalentTo(expected);
+
+        await _booksRepository.DeleteAsync(expected.Id);
+    }
+
+    [Theory, AutoData]
+    public async Task Delete(Book book)
+    {
+        await _booksRepository.CreateAsync(book);
+
+        await _booksRepository.DeleteAsync(book.Id);
+
+        var result = await _booksRepository.GetBookAsync(book.Id);
+
+        result.Should().BeNull();
+    }
+
+    [Theory, AutoData]
+    public async Task GetBook(Book expected)
+    {
+        await _booksRepository.CreateAsync(expected);
+
+        var actual = await _booksRepository.GetBookAsync(expected.Id);
+
+        actual.Should().NotBeNull();
+        actual.Should().BeEquivalentTo(expected);
+
+        await _booksRepository.DeleteAsync(expected.Id);
+    }
+
+    [Theory, AutoData]
+    public async Task GetBook_BookDoesNotExist_ShouldReturnNull(long id)
+    {
+        var result = await _booksRepository.GetBookAsync(id);
+
+        result.Should().BeNull();
     }
 
     [Theory, AutoData]
@@ -49,5 +98,58 @@ public class MongoBookRepositoryTests : BaseIntegrationTest
         await _booksRepository.DeleteAsync(bookA.Id);
         await _booksRepository.DeleteAsync(bookB.Id);
         await _booksRepository.DeleteAsync(bookC.Id);
+    }
+
+    [Fact]
+    public void GetBooks_EmptyDatabase_ShouldReturnEmptyEnumerable()
+    {
+        var books = _booksRepository.GetBooks();
+
+        books.Count().Should().Be(0);
+    }
+
+    [Theory, AutoData]
+    public async Task Update(Book originalBook, Book expected)
+    {
+        expected.Id = 1;
+
+        await _booksRepository.CreateAsync(originalBook);
+
+        await _booksRepository.UpdateAsync(expected);
+
+        var actual = await _booksRepository.GetBookAsync(expected.Id);
+
+        actual.Should().NotBeNull();
+        actual.Should().BeEquivalentTo(expected);
+
+        await _booksRepository.DeleteAsync(actual!.Id);
+    }
+
+    [Theory, AutoData]
+    public async Task Update_BookDoesNotExist_ShouldReturnNull(Book book)
+    {
+        var result = await _booksRepository.UpdateAsync(book);
+
+        result.Should().BeNull();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await CleanDatabase();
+
+        base.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    private async Task CleanDatabase()
+    {
+        var books = _booksRepository.GetBooks();
+        if(books.Any())
+        {
+            foreach(var book in books)
+            {
+                await _booksRepository.DeleteAsync(book.Id);
+            }
+        }
     }
 }
